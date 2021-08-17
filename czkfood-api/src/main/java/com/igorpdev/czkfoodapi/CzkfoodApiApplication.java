454e368542fdff22120dc7913d2c483fbd153c25
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-17 16:09:32 -0300

    Project created

---
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/CzkfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.igorpdev.czkfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzkfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzkfoodApiApplication.class, args);
+	}
+
+}
---
+++ czkfood-api/src/test/java/com/igorpdev/czkfoodapi/CzkfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorpdev.czkfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzkfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

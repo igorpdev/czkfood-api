71551b3f99950ec0fa043a7ccbee9b0afdaffa37
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-19 12:12:36 -0300

    Adding not null property to the database

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/model/Cozinha.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/model/Cozinha.java
@@ -1,5 +1,6 @@
 package com.igorpdev.czkfoodapi.domain.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,6 +21,7 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
 }

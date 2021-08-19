b34bb983432ebdf7110310f2b77c53f14810f415
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-19 16:02:47 -0300

    Handling the HTTP Response with ResponseEntity

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/controller/CozinhaController.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/controller/CozinhaController.java
@@ -6,6 +6,7 @@ import com.igorpdev.czkfoodapi.domain.model.Cozinha;
 import com.igorpdev.czkfoodapi.domain.repository.CozinhaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +25,11 @@ public class CozinhaController {
     }
 
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId) {
-        return cozinhaRepository.buscar(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+        //return ResponseEntity.status(HttpStatus.OK).body(cozinha); //.build() no lugar do body retorna somente o status
+        return ResponseEntity.ok(cozinha);
     }
-    
+
 }

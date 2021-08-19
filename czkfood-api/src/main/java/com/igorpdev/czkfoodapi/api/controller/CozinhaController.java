b31b9bf8a3a6ab3f4add3a1d4e10717000b66a31
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-19 16:35:32 -0300

    Implementing the inclusion of resources with POST

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/controller/CozinhaController.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/controller/CozinhaController.java
@@ -6,10 +6,14 @@ import com.igorpdev.czkfoodapi.domain.model.Cozinha;
 import com.igorpdev.czkfoodapi.domain.repository.CozinhaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -36,6 +40,12 @@ public class CozinhaController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
     }
 
 }

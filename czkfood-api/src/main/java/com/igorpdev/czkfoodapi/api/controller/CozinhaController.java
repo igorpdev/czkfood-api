be42f23c74b9f566eb828267966eaa64bf99e5ce
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-19 15:02:51 -0300

    Creating a Collection Resource with GET

---
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.igorpdev.czkfoodapi.api.controller;
+
+import java.util.List;
+
+import com.igorpdev.czkfoodapi.domain.model.Cozinha;
+import com.igorpdev.czkfoodapi.domain.repository.CozinhaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cozinhas")
+public class CozinhaController {
+    
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+}

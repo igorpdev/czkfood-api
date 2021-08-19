0fc4522f783a4110482738e25924b49608948679
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-19 12:06:26 -0300

    Mapping ManyToOne relationship

--- czkfood-api/src/main/resources/import.sql
+++ czkfood-api/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-insert into tb_cozinhas (nome) values ('Tailandesa');
-insert into tb_cozinhas (nome) values ('Indiana');
+insert into tb_cozinhas (id, nome) values (1, 'Tailandesa');
+insert into tb_cozinhas (id, nome) values (2, 'Indiana');
 
-insert into tb_restaurantes (nome, taxa_frete) values ('Thai Gourmet', 10);
-insert into tb_restaurantes (nome, taxa_frete) values ('Thai Delivery', 9.50);
-insert into tb_restaurantes (nome, taxa_frete) values ('Tuk Tuk Comida Indiana', 15);
+insert into tb_restaurantes (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
+insert into tb_restaurantes (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 1);
+insert into tb_restaurantes (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);

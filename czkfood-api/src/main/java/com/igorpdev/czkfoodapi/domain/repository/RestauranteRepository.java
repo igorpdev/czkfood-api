4d8a3ab7890e982d620e57087b0220f1ff0b0b76
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-22 14:47:15 -0300

    Using custom JPQL queries with @Query

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/repository/RestauranteRepository.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/repository/RestauranteRepository.java
@@ -7,12 +7,17 @@ import java.util.Optional;
 import com.igorpdev.czkfoodapi.domain.model.Restaurante;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
         
     List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
-    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+    @Query("FROM Restaurante WHERE nome LIKE %:nome% AND cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
+    
+    //List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 
     Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
 

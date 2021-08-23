c02657e0dc86c9c78a63b7d730e996ca967f3ca9
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-23 16:57:19 -0300

    Mapping '@ManyToMany' relationships

--- czkfood-api/src/test/java/com/igorpdev/czkfoodapi/CzkfoodApiApplicationTests.java
+++ czkfood-api/src/test/java/com/igorpdev/czkfoodapi/CzkfoodApiApplicationTests.java
@@ -1,12 +1,12 @@
 package com.igorpdev.czkfoodapi;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CzkfoodApiApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 

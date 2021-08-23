075404048d54c510b3a72fe0e30066693a546f54
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-23 18:02:28 -0300

    Mapping Embedded Classes w/ Embedded & Embeddable

--- czkfood-api/src/main/resources/import.sql
+++ czkfood-api/src/main/resources/import.sql
@@ -1,10 +1,6 @@
 insert into tb_cozinhas (id, nome) values (1, 'Tailandesa');
 insert into tb_cozinhas (id, nome) values (2, 'Indiana');
 
-insert into tb_restaurantes (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
-insert into tb_restaurantes (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 1);
-insert into tb_restaurantes (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
-
 insert into tb_estado (id, nome) values (1, 'Minas Gerais');
 insert into tb_estado (id, nome) values (2, 'São Paulo');
 insert into tb_estado (id, nome) values (3, 'Ceará');
@@ -15,6 +11,10 @@ insert into tb_cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
 insert into tb_cidade (id, nome, estado_id) values (4, 'Campinas', 2);
 insert into tb_cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
 
+insert into tb_restaurantes (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Thai Gourmet', 10, 1, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
+insert into tb_restaurantes (id, nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
+insert into tb_restaurantes (id, nome, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 15, 2);
+
 insert into tb_forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
 insert into tb_forma_pagamento (id, descricao) values (2, 'Cartão de débito');
 insert into tb_forma_pagamento (id, descricao) values (3, 'Dinheiro');
@@ -23,3 +23,4 @@ insert into tb_permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 
 insert into tb_permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);    
+  

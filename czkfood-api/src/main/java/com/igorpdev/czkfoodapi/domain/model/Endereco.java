6b4760944ac279bfad9b53e90f17428623f491d7
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-24 14:21:47 -0300

    Changing the fetching strategy to Lazy Loading

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/model/Endereco.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/model/Endereco.java
@@ -2,6 +2,7 @@ package com.igorpdev.czkfoodapi.domain.model;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
@@ -29,7 +30,7 @@ public class Endereco {
     private String bairro;
 
     @JsonIgnore
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "endereco_cidade_id")
     private Cidade cidade;
 

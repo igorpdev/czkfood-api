1069fcc877be59fea2761723897af65cb6337e1f
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-24 11:56:02 -0300

    Creating two-way rel between Restaurante & Produto

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/model/Restaurante.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/model/Restaurante.java
@@ -15,6 +15,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -49,6 +50,10 @@ public class Restaurante {
     @JsonIgnore
     @Embedded
     private Endereco endereco;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "restaurante")
+    private List<Produto> produtos = new ArrayList<>();
 
     @JsonIgnore
     @CreationTimestamp

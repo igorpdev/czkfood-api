2f0c2362a035f7886fe201a0e6a19014cc11f020
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-25 12:06:03 -0300

    Migration V005 & Pedido and ItemPedido entities

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/model/Restaurante.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/model/Restaurante.java
@@ -42,7 +42,7 @@ public class Restaurante {
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
-    @ManyToOne //(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
---
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/model/StatusPedido.java
@@ -0,0 +1,10 @@
+package com.igorpdev.czkfoodapi.domain.model;
+
+public enum StatusPedido {
+    
+    CRIADO,
+    CONFIRMADO,
+    ENTREGUE,
+    CANCELADO
+    
+}

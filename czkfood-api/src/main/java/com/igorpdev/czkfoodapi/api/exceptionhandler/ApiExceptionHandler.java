d82445840d4bb260118471896e96a897443c346d
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-25 17:05:10 -0300

    Implementing ResponseEntityExceptionHandler

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,12 +8,12 @@ import com.igorpdev.czkfoodapi.domain.exception.NegocioException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
@@ -41,15 +41,5 @@ public class ApiExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
     }
-
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> handleHttpMediaTypeNotSupportedException() {
-        Problema problema = Problema.builder()
-            .dataHora(LocalDateTime.now())
-            .mensagem("O tipo de mídia não é aceito").build();
-
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema);
-    }
-    
 
 }

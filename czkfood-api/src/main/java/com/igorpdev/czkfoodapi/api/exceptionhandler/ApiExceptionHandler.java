f6b7dec8736ec5f568dd685526f8969d882065f4
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-26 11:40:27 -0300

    Formatting excep w/ the RFC 7807 response standard

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.igorpdev.czkfoodapi.api.exceptionhandler;
 
-import java.time.LocalDateTime;
-
 import com.igorpdev.czkfoodapi.domain.exception.EntidadeEmUsoException;
 import com.igorpdev.czkfoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.igorpdev.czkfoodapi.domain.exception.NegocioException;
@@ -20,8 +18,13 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
         
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(),
-            HttpStatus.NOT_FOUND, request);
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
+        String detail = ex.getMessage();
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
@@ -43,16 +46,24 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
             HttpStatus status, WebRequest request) {
 
         if (body == null) {
-            body = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(status.getReasonPhrase()).build();
+            body = Problem.builder()
+                .title(status.getReasonPhrase())
+                .status(status.value()).build();
         } else if (body instanceof String) {
-            body = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem((String)body).build();
+            body = Problem.builder()
+                .title((String)body)
+                .status(status.value()).build();
         }
 
         return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
+        return Problem.builder()
+        .status(status.value())
+        .type(problemType.getUri())
+        .title(problemType.getTitle())
+        .detail(detail);
     }
 
 }

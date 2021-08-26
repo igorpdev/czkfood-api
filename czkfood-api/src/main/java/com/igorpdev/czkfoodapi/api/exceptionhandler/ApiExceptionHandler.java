04b0ba979924a5f8cb31c5b209ea451ae4937cc2
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-26 14:15:38 -0300

    Handling PropertyBindingExcep in deserialization

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.igorpdev.czkfoodapi.api.exceptionhandler;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import com.igorpdev.czkfoodapi.domain.exception.EntidadeEmUsoException;
 import com.igorpdev.czkfoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.igorpdev.czkfoodapi.domain.exception.NegocioException;
@@ -27,6 +30,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         Throwable rootCause = ExceptionUtils.getRootCause(ex);
         if (rootCause instanceof InvalidFormatException) {
             return handleInvalidFormatException((InvalidFormatException)rootCause, headers, status, request);
+        } else if (rootCause instanceof PropertyBindingException) {
+            return handlePropertyBindingException((PropertyBindingException) rootCause, headers, status, request); 
         }
         
         ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
@@ -40,9 +45,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex,
     HttpHeaders headers, HttpStatus status, WebRequest request) {
         
-        String path = ex.getPath().stream()
-            .map(ref -> ref.getFieldName())
-            .collect(Collectors.joining("."));
+        String path = joinPath(ex.getPath());
         
         ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
         String detail = String.format("A propriedade '%s' recebeu o valor: '%s', "
@@ -52,6 +55,26 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         
         return handleExceptionInternal(ex, problem, headers, status, request);
     }
+
+    private ResponseEntity<Object> handlePropertyBindingException(PropertyBindingException ex,
+    HttpHeaders headers, HttpStatus status, WebRequest request) {
+        
+        String path = joinPath(ex.getPath());
+        
+        ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+        String detail = String.format("A propriedade '%s' não existe. "
+            + "Corrija ou remova essa propriedade e tente novamente.", path);
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+        
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+
+    private String joinPath(List<Reference> references) {
+        return references.stream()
+            .map(ref -> ref.getFieldName())
+            .collect(Collectors.joining("."));
+    }   
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {

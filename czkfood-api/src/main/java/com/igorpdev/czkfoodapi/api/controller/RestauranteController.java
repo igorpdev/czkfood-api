91f33bd7a9560008a9dcb53363057af5a25ebe62
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-26 14:36:47 -0300

    Throwing Deserialization Excep in Partial Update

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/controller/RestauranteController.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/controller/RestauranteController.java
@@ -4,6 +4,9 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.igorpdev.czkfoodapi.domain.exception.CozinhaNaoEncontradaException;
 import com.igorpdev.czkfoodapi.domain.exception.NegocioException;
@@ -11,9 +14,12 @@ import com.igorpdev.czkfoodapi.domain.model.Restaurante;
 import com.igorpdev.czkfoodapi.domain.repository.RestauranteRepository;
 import com.igorpdev.czkfoodapi.domain.service.CadastroRestauranteService;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -73,26 +79,40 @@ public class RestauranteController {
         se o consumidor desejasse atualizar algum valor para NULL, utilizando o PATCH direto no
             Objeto, este NULL seria ignorado, pois o PATCH só considera valores "válidos" */
     @PatchMapping("/{restauranteId}")
-    public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+    public Restaurante atualizarParcial(@PathVariable Long restauranteId,
+            @RequestBody Map<String, Object> campos, HttpServletRequest request) {
+        
         Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
-        merge(campos, restauranteAtual);
+        merge(campos, restauranteAtual, request);
 
         return atualizar(restauranteId, restauranteAtual);
     }
 
-    private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-		ObjectMapper objectMapper = new ObjectMapper(); //responsável por converter os valores inseridos para os aceitos na API. exemplo Int -> BigDecimal
-        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
-        
-        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade); // findField retorna a instância de um campo
-            field.setAccessible(true);
+    private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino,
+            HttpServletRequest request) {
 
-            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem); // getField retorna o valor de um campo
+        ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request); 
 
-            ReflectionUtils.setField(field, restauranteDestino, novoValor);
-		});
+		try {
+            ObjectMapper objectMapper = new ObjectMapper(); //responsável por converter os valores inseridos para os aceitos na API. exemplo Int -> BigDecimal
+            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+    
+            Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+            
+            dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+                Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade); // findField retorna a instância de um campo
+                field.setAccessible(true);
+    
+                Object novoValor = ReflectionUtils.getField(field, restauranteOrigem); // getField retorna o valor de um campo
+    
+                ReflectionUtils.setField(field, restauranteDestino, novoValor);
+            });
+        } catch (IllegalArgumentException e) {
+            Throwable rootCause = ExceptionUtils.getRootCause(e);
+            throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+        }
 	}
 
 }

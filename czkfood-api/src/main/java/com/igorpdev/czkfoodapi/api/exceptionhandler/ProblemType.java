d237c3791d0fe741a8ed21708099e9b607e89814
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-27 08:45:57 -0300

    Validation constraint violation exception handling

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/exceptionhandler/ProblemType.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
     
+    DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
     ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
     RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),

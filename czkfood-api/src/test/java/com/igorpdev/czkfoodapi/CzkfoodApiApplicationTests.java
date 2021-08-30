6391dd9b5faba0e62b5136625a352997b95c6bae
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-30 12:57:55 -0300

    Preparing the project for integration tests

--- czkfood-api/src/test/java/com/igorpdev/czkfoodapi/CzkfoodApiApplicationTests.java
+++ czkfood-api/src/test/java/com/igorpdev/czkfoodapi/CzkfoodApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.igorpdev.czkfoodapi;
 
-//import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class CzkfoodApiApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
+		assertFalse(true);
 	}
 
 }

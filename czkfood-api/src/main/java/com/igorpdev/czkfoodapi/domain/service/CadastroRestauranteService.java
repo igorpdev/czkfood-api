fd7beade939f8e4b41dfc69db6751e3b3b77711b
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-08-31 16:03:32 -0300

    Better defining the scope of transactionals

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/service/CadastroRestauranteService.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/service/CadastroRestauranteService.java
@@ -7,6 +7,7 @@ import com.igorpdev.czkfoodapi.domain.repository.RestauranteRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CadastroRestauranteService {
@@ -17,6 +18,7 @@ public class CadastroRestauranteService {
     @Autowired
     CadastroCozinhaService cadastroCozinha;
 
+    @Transactional
     public Restaurante salvar(Restaurante restaurante) {
         Long cozinhaId = restaurante.getCozinha().getId();
 

d260378375d90a3cef6ac040eb2e97755dae3fbf
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-09-06 12:56:29 -0300

    Add Endereco in the Restaurante resrc rep template

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/core/modelmapper/ModelMapperConfig.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/core/modelmapper/ModelMapperConfig.java
@@ -1,5 +1,8 @@
 package com.igorpdev.czkfoodapi.core.modelmapper;
 
+import com.igorpdev.czkfoodapi.api.model.EnderecoModel;
+import com.igorpdev.czkfoodapi.domain.model.Endereco;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +12,14 @@ public class ModelMapperConfig {
     
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        var modelMapper = new ModelMapper();
+
+        var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+        enderecoToEnderecoModelTypeMap.<String>addMapping(
+            enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(), 
+            (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
+
+        return modelMapper;
     }
 
 }

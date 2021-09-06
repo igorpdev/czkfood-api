3db65496fdf6e7a59d4eb689dbdfc486c50aa67a
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-09-06 12:31:55 -0300

    Implementing the "FormaPagamento" endpoints

---
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/api/assembler/FormaPagamentoModelAssembler.java
@@ -0,0 +1,29 @@
+package com.igorpdev.czkfoodapi.api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.igorpdev.czkfoodapi.api.model.FormaPagamentoModel;
+import com.igorpdev.czkfoodapi.domain.model.FormaPagamento;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FormaPagamentoModelAssembler {
+    
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    public FormaPagamentoModel toModel(FormaPagamento formaPagamento) {
+        return modelMapper.map(formaPagamento, FormaPagamentoModel.class);
+    }
+    
+    public List<FormaPagamentoModel> toCollectionModel(List<FormaPagamento> formasPagamento) {
+        return formasPagamento.stream()
+                .map(formaPagamento -> toModel(formaPagamento))
+                .collect(Collectors.toList());
+    }
+
+}

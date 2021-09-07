56c8921f130de14673209ba3b17f08f88fbecdbe
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-09-07 17:05:21 -0300

    Implementing 'Pedido' status transition endpoints

---
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/service/FluxoPedidoService.java
@@ -0,0 +1,61 @@
+package com.igorpdev.czkfoodapi.domain.service;
+
+import java.time.OffsetDateTime;
+
+import com.igorpdev.czkfoodapi.domain.exception.NegocioException;
+import com.igorpdev.czkfoodapi.domain.model.Pedido;
+import com.igorpdev.czkfoodapi.domain.model.StatusPedido;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class FluxoPedidoService {
+
+    @Autowired
+    private EmissaoPedidoService emissaoPedido;
+    
+    @Transactional
+    public void confirmar(Long pedidoId) {
+        Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+
+        if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+            throw new NegocioException(
+                String.format("Status do pedido %d não pode ser alterado de %s para %s", 
+                    pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CONFIRMADO.getDescricao()));
+        }
+
+        pedido.setStatus(StatusPedido.CONFIRMADO);
+        pedido.setDataConfirmacao(OffsetDateTime.now());
+    }
+
+    @Transactional
+    public void cancelar(Long pedidoId) {
+        Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+
+        if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+            throw new NegocioException(
+                String.format("Status do pedido %d não pode ser alterado de %s para %s", 
+                    pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CANCELADO.getDescricao()));
+        }
+
+        pedido.setStatus(StatusPedido.CANCELADO);
+        pedido.setDataConfirmacao(OffsetDateTime.now());
+    }
+
+    @Transactional
+    public void entregar(Long pedidoId) {
+        Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+
+        if (!pedido.getStatus().equals(StatusPedido.CONFIRMADO)) {
+            throw new NegocioException(
+                String.format("Status do pedido %d não pode ser alterado de %s para %s", 
+                    pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.ENTREGUE.getDescricao()));
+        }
+
+        pedido.setStatus(StatusPedido.ENTREGUE);
+        pedido.setDataConfirmacao(OffsetDateTime.now());
+    }
+
+}

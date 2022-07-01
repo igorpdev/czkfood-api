5e6c5fdd13666f46446eaad4362a425f03862df0
Igor Pereira <igorjps@uol.com.br>
2022-07-01 17:12:18 -0300

    Implementing removal and replacement of photo files in the storage service

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/infrastructure/service/storage/LocalFotoStorageService.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/infrastructure/service/storage/LocalFotoStorageService.java
@@ -31,4 +31,16 @@ public class LocalFotoStorageService implements FotoStorageService {
         return diretorioFotos.resolve(Path.of(nomeArquivo));
     }
 
+    @Override
+    public void remover(String nomeArquivo) {
+        try {
+            Path arquivoPath = getArquivoPath(nomeArquivo);
+
+            Files.deleteIfExists(arquivoPath);
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível excluir o arquivo", e);
+        }
+        
+    }
+
 }

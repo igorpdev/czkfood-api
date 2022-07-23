bc4668dfbbf7f5fee16d6421e8edc33308c82de4
Igor Pereira <igorjps@uol.com.br>
2022-07-23 16:23:30 -0300

    Implementing Photo Recovery in S3 Storage Service

--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/service/FotoStorageService.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/domain/service/FotoStorageService.java
@@ -20,7 +20,7 @@ public interface FotoStorageService {
         }
     }
 
-    InputStream recuperar(String nomeArquivo);
+    FotoRecuperada recuperar(String nomeArquivo);
 
     default String gerarNomeArquivo(String nomeOriginal) {
         return UUID.randomUUID().toString() + "_" + nomeOriginal;
@@ -33,6 +33,23 @@ public interface FotoStorageService {
         private String nomeArquivo;
         private String contentType;
         private InputStream inputStream;
+
+    }
+
+    @Getter
+    @Builder
+    class FotoRecuperada {
+
+        private InputStream inputStream;
+        private String url;
+
+        public boolean temUrl() {
+            return url != null;
+        }
+
+        public boolean temInputStream() {
+            return inputStream != null;
+        }
 
     }
 
--- czkfood-api/src/main/java/com/igorpdev/czkfoodapi/infrastructure/service/storage/S3FotoStorageService.java
+++ czkfood-api/src/main/java/com/igorpdev/czkfoodapi/infrastructure/service/storage/S3FotoStorageService.java
@@ -1,6 +1,6 @@
 package com.igorpdev.czkfoodapi.infrastructure.service.storage;
 
-import java.io.InputStream;
+import java.net.URL;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,27 +45,33 @@ public class S3FotoStorageService implements FotoStorageService {
 
     }
 
-    private String getCaminhoArquivo(String nomeArquivo) {
-        return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
-    }
-
     @Override
     public void remover(String nomeArquivo) {
         try {
             String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
-
+            
             var deleteObjectRequest = new DeleteObjectRequest(
-                    storageProperties.getS3().getBucket(), caminhoArquivo);
+                storageProperties.getS3().getBucket(), caminhoArquivo);
 
-            amazonS3.deleteObject(deleteObjectRequest);
-        } catch (Exception e) {
-            throw new StorageException("Não foi possível excluir arquivo na Amazon S3", e);
+                amazonS3.deleteObject(deleteObjectRequest);
+            } catch (Exception e) {
+                throw new StorageException("Não foi possível excluir arquivo na Amazon S3", e);
+            }
         }
-    }
+        
+        @Override
+        public FotoRecuperada recuperar(String nomeArquivo) {
+            String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
 
-    @Override
-    public InputStream recuperar(String nomeArquivo) {
-        return null;
-    }
+            URL url = amazonS3.getUrl(storageProperties.getS3().getBucket(), caminhoArquivo);
+            
+            return FotoRecuperada.builder()
+                    .url(url.toString()).build();
+        }
+        
+        private String getCaminhoArquivo(String nomeArquivo) {
+            return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
+        }
 
-}
+    }
+    
